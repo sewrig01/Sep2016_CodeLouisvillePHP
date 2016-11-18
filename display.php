4961fe78c7c3434073c224c42d948ddf4c3abb3e
Stephen Wright <Stephenewright@gmail.com>
2016-11-17 23:32:04 -0500

    Greyed out inactive items. Out of time for more features etc,

--- display.php
+++ display.php
@@ -7,18 +7,32 @@
 #  }
 
   function display_all($id, $item){
-
-    echo "<form method = 'post' form action = 'q.php'>
-    Exercise :<input type ='text' class = 'tboxLG' name = 'FormName' value = '".$item["ExerciseName"]."'<br>
-    Weight :<input type ='text' class = 'tboxMD' name = 'FormWeight' value = '".$item["Weight"]."'<br>
-      Sets :<input type ='text' class = 'tboxSM' name = 'FormSets' value = '".$item["Sets"]."'<br>
-        Reps :<input type ='text' class = 'tboxSM' name = 'FormReps' value = '".$item["Reps"]."'<br>
-          Last Date :<input type ='text' class = 'tboxLG' name = 'FormLastDate' value = '".$item["LastDate"]."'<br>
-          MG:<input type = 'text' class = 'tboxMD' name = 'FormMG' value = '".$item["MG"]."'<br>
-          Active:<input type = 'text' class = 'tboxSM' name = 'FormAc' value = '".$item["Active"]."'<br>
+if ($item["Active"]!='y'){
+    echo "<form method = 'post' form action = 'q.php'  class ='inactive'>
+    Exercise :<input type ='text' class = 'tboxLG inactive' name = 'FormName' value = '".$item["ExerciseName"]."'<br>
+    Weight :<input type ='text' class = 'tboxMD inactive' name = 'FormWeight' value = '".$item["Weight"]."'<br>
+      Sets :<input type ='text' class = 'tboxSM inactive' name = 'FormSets' value = '".$item["Sets"]."'<br>
+        Reps :<input type ='text' class = 'tboxSM inactive' name = 'FormReps' value = '".$item["Reps"]."'<br>
+          Last Date :<input type ='text' class = 'tboxLG inactive' name = 'FormLastDate' value = '".$item["LastDate"]."'<br>
+          MG:<input type = 'text' class = 'tboxMD inactive' name = 'FormMG' value = '".$item["MG"]."'<br>
+          Active:<input type = 'text' class = 'tboxSM inactive' name = 'FormAc' value = '".$item["Active"]."'<br>
         id:<input type ='hidden' name = 'FormId' value = '".$item["id"]."'<br>
         <input type='submit' value = 'update'>
-        </form>";
+        </form>";}
+        else{echo "<form method = 'post' form action = 'q.php'>
+        Exercised :<input type ='text' class = 'tboxLG' name = 'FormName' value = '".$item["ExerciseName"]."'<br>
+        Weight :<input type ='text' class = 'tboxMD' name = 'FormWeight' value = '".$item["Weight"]."'<br>
+          Sets :<input type ='text' class = 'tboxSM' name = 'FormSets' value = '".$item["Sets"]."'<br>
+            Reps :<input type ='text' class = 'tboxSM' name = 'FormReps' value = '".$item["Reps"]."'<br>
+              Last Date :<input type ='text' class = 'tboxLG' name = 'FormLastDate' value = '".$item["LastDate"]."'<br>
+              MG:<input type = 'text' class = 'tboxMD' name = 'FormMG' value = '".$item["MG"]."'<br>
+              Active:<input type = 'text' class = 'tboxSM' name = 'FormAc' value = '".$item["Active"]."'<br>
+            id:<input type ='hidden' name = 'FormId' value = '".$item["id"]."'<br>
+            <input type='submit' value = 'update'>
+            </form>";}
+
+
+
 }
 
 ?>
